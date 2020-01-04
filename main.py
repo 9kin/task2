de786e6c30ee16c560384b62f4e0fd73d4320fd1
9kin <gate.web.tester@mail.ru>
2020-01-04 16:10:58 +0500

    first commit

---
+++ main.py
@@ -0,0 +1,51 @@
+import sqlite3
+import sys
+import sqlite3
+from PyQt5.QtGui import QPixmap, QImage
+from PyQt5.QtWidgets import (QWidget, QPushButton,
+                             QHBoxLayout, QVBoxLayout, QApplication)
+
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton
+from PyQt5.QtWidgets import (QMainWindow, QLabel, QLCDNumber, QMessageBox,
+                             QCheckBox, QPlainTextEdit, QInputDialog,  QLineEdit, QTableWidget,
+                             QTableWidgetItem, QSpinBox, QVBoxLayout, QColorDialog, QGridLayout)
+from PyQt5.QtGui import QPainter, QBrush, QPen, QColor
+from PyQt5.QtCore import Qt, pyqtSlot, QByteArray
+
+
+from PyQt5 import uic
+from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow
+import random
+
+class Example(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('UI.ui',self)
+        self.pushButton.move(200, 200)
+        self.pushButton.setFixedSize(100, 100)
+        self.objects = []
+        self.pushButton.clicked.connect(self.new)
+        self.setFixedSize(500, 500)
+
+    def new(self):
+        r = random.randrange(20, 200)
+        x, y = random.randrange(0, 500), random.randrange(0, 500)
+        self.objects.append([x, y, r])
+        self.update()
+
+    def paintEvent(self, event):
+        qp = QPainter()
+        qp.begin(self)
+        qp.setBrush(QBrush(Qt.yellow, Qt.SolidPattern))
+        self.drawFlag(qp)
+        qp.end()
+
+    def drawFlag(self, qp):
+        for i in self.objects:
+            qp.drawEllipse(i[0], i[1], i[2], i[2])
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = Example()
+    ex.show()
+    sys.exit(app.exec_())
